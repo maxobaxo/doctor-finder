0fdbe9fa84b84c9f6f90d188937b76da54cd59a6
Max Scher <maxobaxo@gmail.com>
2017-08-04 08:20:40 -0700

    add basic html with form and corresponding UI js

---
+++ js/doctor-interface.js
@@ -0,0 +1,9 @@
+var apiKey = require('./../.env').apiKey;
+
+$(document).ready(function() {
+  $('#issue-form').submit(function(event) {
+    event.preventDefault();
+    var symptom = $('#issue-input').val();
+    
+  });
+});
