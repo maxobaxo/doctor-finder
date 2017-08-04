9765844d355bcd465cc44b101fe46d7263124cf5
Max Scher <maxobaxo@gmail.com>
2017-08-04 14:24:46 -0700

    add sort order to submit form

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,43 +1,58 @@
 var apiKey = require('./../.env').apiKey;
 var DoctorModule = require('./../js/doctor.js').doctorModule;
 
-// function displayPractices(doctorsArr) {
-//     var doctorsArr
-// });
-
 function displayDoctors(doctorsArr) {
-  doctorsArr.forEach(function(doctor) {
+  $('#doctor-list').empty();
+  if (doctorsArr.length === 0) {
     $('#doctor-list').append(
       '<div class="well">' +
-        '<h3>' +
-        doctor.profile.first_name + " " + doctor.profile.last_name +
-        '</h3>' +
-        '<h4> Primary Specialty: ' + doctor.specialties[0].name + '</h4>' +
-        '<h4> Practices:</h4>' +
-        '<div id="practices-' + doctor.uid +'">' + '</div>' +
+      '<h3>' +
+      'Your search has returned no results.' +
+      '</h3>' +
       '</div>'
     );
+  } else {
+    doctorsArr.forEach(function(doctor) {
+      $('#doctor-list').append(
+        '<div class="well">' +
+          '<div class="row">' +
+            '<div class="col-md-6">' +
+              '<h3>' +
+                doctor.profile.first_name + " " + doctor.profile.last_name + ", " +
+                doctor.profile.title +
+              ' (' + doctor.specialties[0].name +
+              ')</h3>' +
+              '<h4> Practice Locations:</h4>' +
+              '<div id="practices-' + doctor.uid +'">' + '</div>' +
+            '</div>' +
+            '<div class="col-md-6">' +
+              '<div id="map">' +
+              '</div>' +
+            '</div>' +
+          '</div>' +
+        '</div>'
+      );
 
-    var practicesArr = [];
-    doctor.practices.forEach(function(practice) {
-      console.log(practice);
-      practicesArr.push(practice);
-    });
+      var practicesArr = [];
+      doctor.practices.forEach(function(practice) {
+        practicesArr.push(practice);
+      });
 
-    practicesArr.forEach(function(practice) {
-      $('#practices-' + doctor.uid).append(
-      practice.name +
-      '<ul>' +
-        '<li>' +
+      practicesArr.forEach(function(practice) {
+        $('#practices-' + doctor.uid).append(
+          practice.name +
+          '<ul>' +
+          '<li>' +
           practice.visit_address.street + '<br>' +
           practice.visit_address.city + ', ' +
           practice.visit_address.state + " " +
           practice.visit_address.zip +
-        '</li>' +
-      '</ul>'
-      );
+          '</li>' +
+          '</ul>'
+        );
+      });
     });
-  });
+  }
 }
 
 $(document).ready(function() {
@@ -46,6 +61,7 @@ $(document).ready(function() {
 
     var doctorModule = new DoctorModule();
     var medicalIssue = $('#issue-input').val();
-    doctorModule.getDoctors(medicalIssue, displayDoctors);
+    var sortOrder = $('#sort-order').val();
+    doctorModule.getDoctors(medicalIssue, displayDoctors, sortOrder);
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -4,12 +4,22 @@ function DoctorModule() {
 
 }
 
-DoctorModule.prototype.getDoctors = function(medicalIssue, displayDoctors) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=45.5231%2C-122.6765%2C205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+DoctorModule.prototype.getDoctors = function(medicalIssue, displayDoctors, sortOrder) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&sort=' + sortOrder + '&location=45.5231%2C-122.6765%2C205&user_location=45.5231%2C-122.6765&user_key=' + apiKey)
   .then(function(response) {
     var doctorsArr = response.data;
     displayDoctors(doctorsArr);
-    console.log(doctorsArr);
+
+    // var Portland = {lat: 45.5207, lng: -122.677397};
+    // var mapObject = new google.maps.Map(document.getElementById('map'), {
+    //   zoom: 11,
+    //   center: Portland,
+    //   mapTypeId: 'terrain'
+    // });
+
+    // for (i = 0; i < doctorsArr.length; i++) {
+    //
+    // }
   })
   .fail(function(error){
     console.log("fail");
