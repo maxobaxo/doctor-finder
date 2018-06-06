7c5cb14eab02aa4030ed905f415060929d938628
Max Scher <maxobaxo@gmail.com>
2018-06-05 20:46:43 -0700

    add https protocol to accommodate firebase ssl

--- js/doctor.js
+++ js/doctor.js
@@ -1,52 +1,70 @@
-var apiKey = require('./../.env').apiKey;
+var apiKey = require("./../.env").apiKey;
 
-function DoctorModule() {
+function DoctorModule() {}
 
-}
+DoctorModule.prototype.getDoctors = function(
+  medicalIssue,
+  displayDoctors,
+  sortOrder,
+  doctorName
+) {
+  $.get(
+    "https://api.betterdoctor.com/2016-03-01/doctors?name=" +
+      doctorName +
+      "&query=" +
+      medicalIssue +
+      "&sort=" +
+      sortOrder +
+      "&location=45.5231%2C-122.6765%2C205&user_location=45.5231%2C-122.6765&user_key=" +
+      apiKey
+  )
+    .then(function(response) {
+      var doctorsArr = response.data;
+      displayDoctors(doctorsArr);
 
-DoctorModule.prototype.getDoctors = function(medicalIssue, displayDoctors, sortOrder, doctorName) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?name=' + doctorName + '&query=' + medicalIssue + '&sort=' + sortOrder + '&location=45.5231%2C-122.6765%2C205&user_location=45.5231%2C-122.6765&user_key=' + apiKey)
-  .then(function(response) {
-    var doctorsArr = response.data;
-    displayDoctors(doctorsArr);
-
-    var Portland = {lat: 45.5207, lng: -122.677397};
-    var mapObject = new google.maps.Map(document.getElementById('map'), {
-      zoom: 9,
-      center: Portland,
-      mapTypeId: 'terrain'
-    });
-
-    doctorsArr.forEach(function(doctor) {
-      var practicesArr = [];
-      doctor.practices.forEach(function(practice) {
-        practicesArr.push(practice);
+      var Portland = { lat: 45.5207, lng: -122.677397 };
+      var mapObject = new google.maps.Map(document.getElementById("map"), {
+        zoom: 9,
+        center: Portland,
+        mapTypeId: "terrain"
       });
 
-      practicesArr.forEach(function(practice) {
-        var address = practice.visit_address.street + " " +
-          practice.visit_address.city + ', ' +
-          practice.visit_address.state + " " +
-          practice.visit_address.zip;
+      doctorsArr.forEach(function(doctor) {
+        var practicesArr = [];
+        doctor.practices.forEach(function(practice) {
+          practicesArr.push(practice);
+        });
 
-        $.get("http://maps.googleapis.com/maps/api/geocode/json?address=" + address)
-          .then(function(response) {
-            // console.log(response.results);
+        practicesArr.forEach(function(practice) {
+          var address =
+            practice.visit_address.street +
+            " " +
+            practice.visit_address.city +
+            ", " +
+            practice.visit_address.state +
+            " " +
+            practice.visit_address.zip;
+
+          $.get(
+            "https://maps.googleapis.com/maps/api/geocode/json?address=" +
+              address
+          ).then(function(response) {
+            console.log(response.results);
             var latitude = response.results[0].geometry.location.lat;
             var longitude = response.results[0].geometry.location.lng;
             var marker = new google.maps.Marker({
-              position: {lat: latitude, lng: longitude},
+              position: { lat: latitude, lng: longitude },
               map: mapObject
             });
             // console.log(response.lat);
             // console.log(response.lng);
           });
+        });
       });
+    })
+    .fail(function(error) {
+      console.log("fail");
     });
-  })
-  .fail(function(error){
-    console.log("fail");
-  });
 };
 
 // DoctorModule.prototype.createMap = function() {
